4d6ede7e56d2fead6590aebc4fed71a003cd203b
hefes10 <martin.miret@gmail.com>
2019-03-30 17:58:20 -0300

    create el principal

--- application/views/principal.php
+++ application/views/principal.php
@@ -1,10 +1,32 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>ASD</title>
-  </head>
-  <body>
-    <p>Hola Mundo</p>
-  </body>
-</html>
+<body>
+<!-- Main -->
+<main id="main">
+	<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+		<ol class="carousel-indicators">
+			<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+			<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+			<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+		</ol>
+		<div class="carousel-inner">
+			<div class="carousel-item active">
+				<img src="assets/img/bici1.jpeg" class="d-block w-100" alt="bici1">
+			</div>
+			<div class="carousel-item">
+				<img src="assets/img/bici2.jpg" class="d-block w-100" alt="bici2">
+			</div>
+			<div class="carousel-item">
+				<img src="assets/img/bici3.jpg" class="d-block w-100" alt="bici3">
+			</div>
+		</div>
+		<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+			<span class="sr-only">Previous</span>
+		</a>
+		<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+			<span class="carousel-control-next-icon" aria-hidden="true"></span>
+			<span class="sr-only">Next</span>
+		</a>
+	</div>
+
+</main>
+<!-- /main -->
