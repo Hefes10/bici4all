2a19e388c383642400977b440489c5d68f512e5d
hefes10 <martin.miret@gmail.com>
2019-03-30 17:57:27 -0300

    create el navbar

---
+++ application/views/front/navbar_view.php
@@ -0,0 +1,55 @@
+<!-- Header -->
+<header>
+  <nav id="navBar" class="navbar navbar-expand-md navbar-light bg-light" style="background: linear-gradient(90deg, #062609 0, #0e4714 25%, #1ab228)">
+    <div class="container">
+      <a class="navbar-brand">
+        <span class="navbar-logo">
+          <a href="<?php echo base_url('principal');?>">
+            <img id="logo" src="assets/img/logo.png" alt="Bici444all" title="" style="height: 2.8rem;">
+          </a>
+        </span>
+      </a>
+
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Productos<span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Nosotros</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Login</a>
+          </li>
+          <li class="nav-item">
+            <a id="comprar" class="nav-link" href="#">
+              <i class="fa fa-shopping-cart"></i>
+            </a>
+          </li>
+          <!-- <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Dropdown
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li> -->
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="search" placeholder="¿Qué estás buscando?" aria-label="Search">
+          <button class="btn btn-dark my-2 my-sm-0" type="submit">
+            <i class="fa fa-search"> Buscar</i>
+          </button>
+        </form>
+      </div>
+    </div>
+  </nav>
+</header>
+<!-- /header -->
