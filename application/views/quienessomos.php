496760ac4454caf9b030831450e09debd2436904
hefes10 <martin.miret@gmail.com>
2019-03-31 20:00:44 -0300

    quienes somos

---
+++ application/views/quienessomos.php
@@ -0,0 +1,18 @@
+<body>
+  <main>
+    <div class="container">
+      <div id="quienessomos">
+        <h1>¿Quiénes somos?</h1>
+        <p>Somos una empresa argentina fundada hace más de 40 años. Nos especializamos en la comercialización de bicicletas y scooters electricos.
+
+        Diseñamos propuestas que privilegian el bienestar, la simpleza y el disfrute de las pequeñas cosas. Cada uno de nuestros productos está pensado para el transporte y disfrute de nuestros clientes.
+
+        Somos una marca dinámica y en constante evolución, nuestra fuerza radica en la motivación y el compromiso de las personas que trabajamos en Bici4all.
+
+        La innovación es uno de nuestros pilares, y nos empuja a buscar nuevas propuestas para asegurarle al público la mejor experiencia, dentro y fuera de nuestras tiendas.
+
+        Nos consideramos responsables del entorno, por eso llevamos adelante una diversidad de propuestas enfocadas en nuestros colaboradores y en la comunidad en la cual nos desarrollamos.</p>
+      </div>
+    </div>
+    <!-- /main -->
+  </main>
