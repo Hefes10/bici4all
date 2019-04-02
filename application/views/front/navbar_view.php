907e6a01b03d6cb506839c0c2a8a8ae222026312
hefes10 <martin.miret@gmail.com>
2019-04-02 20:34:18 -0300

    arreglado el boton de busqueda

--- application/views/front/navbar_view.php
+++ application/views/front/navbar_view.php
@@ -42,7 +42,7 @@
       </li> -->
         </ul>
         <form class="form-inline my-2 my-lg-0">
-          <input class="form-control mr-sm-2" type="search" placeholder="¿Qué estás buscando?" aria-label="Search">
+          <input class="form-control mr-2" type="search" placeholder="¿Qué estás buscando?" aria-label="Search">
           <button class="btn btn-dark my-2 my-sm-0" type="submit">
             <i class="fa fa-search"></i>
           </button>
