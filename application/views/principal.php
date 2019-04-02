c4c3e5f0c6eae301029045b51ea65442a3dfc280
hefes10 <martin.miret@gmail.com>
2019-04-02 19:34:42 -0300

    agregado de algunas cards y no se que

--- application/views/principal.php
+++ application/views/principal.php
@@ -30,11 +30,120 @@
 	</div>
 	<!-- /carousel -->
 
-<!-- Cards -->
-	<div class="container">
-
-	</div>
-<!-- /Cards -->
+	<!-- Cards -->
+	<section id="productos" class="mt-4">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6 col-lg-4  mb-4">
+					<div class="card">
+					  <img src="assets/img/imagen1.jpg" class="card-img-top" alt="...">
+					  <div class="card-body">
+					    <h5 class="card-title">EFE Freego Scooter</h5>
+					    <p class="card-text">
+								Marca: Freego
+								<br>
+								Potencia: 200 - 250w
+								<br>
+								Velocidad máx.: 25km/h
+								<br>
+								Voltaje: 30V
+								<br>
+								Bateria: 4800 mAh litio 208Wh
+								<br>
+								Peso: 10,5 kg
+							</p>
+					    <a href="#" class="btn btn-primary">Comprar</a>
+				  	</div>
+				</div>
+				</div>
+				<div class="col-md-6 col-lg-4  mb-4">
+					<div class="card">
+					  <img src="assets/img/imagen2.jpg" class="card-img-top" alt="...">
+					  <div class="card-body">
+					    <h5 class="card-title">B12</h5>
+							<p class="card-text">
+								Marca: OVER
+								<br>
+								Potencia: 251 - 350w
+								<br>
+								Velocidad máx.: 35-38 km/h
+								<br>
+								Voltaje: 36V
+								<br>
+								Bateria: 36 V 13AH Samsung
+								<br>
+								Peso: 16 kg
+							</p>
+					    <a href="#" class="btn btn-primary">Comprar</a>
+				  	</div>
+				</div>
+				</div>
+				<div class="col-md-6 col-lg-4  mb-4">
+					<div class="card">
+					  <img src="assets/img/imagen3.png" class="card-img-top" alt="...">
+					  <div class="card-body">
+					    <h5 class="card-title">RAZOR-132</h5>
+							<p class="card-text">
+								Marca: RAZOR
+								<br>
+								Potencia: 200 - 250w
+								<br>
+								Velocidad máx.: 30 km/h
+								<br>
+								Voltaje: 36V
+								<br>
+								Bateria: LG batería 5.3Ah ~ 9.9Ah
+								<br>
+								Peso: 16 kg
+							</p>
+					    <a href="#" class="btn btn-primary">Comprar</a>
+				  	</div>
+				</div>
+				</div>
+				<div class="col-md-6 col-lg-4  mb-4">
+					<div class="card">
+					  <img src="assets/img/imagen4.png" class="card-img-top" alt="...">
+					  <div class="card-body">
+					    <h5 class="card-title">Card title</h5>
+					    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					    <a href="#" class="btn btn-primary">Comprar</a>
+				  	</div>
+				</div>
+				</div>
+				<div class="col-md-6 col-lg-4  mb-4">
+					<div class="card">
+					  <img src="assets/img/imagen5.jpg" class="card-img-top" alt="...">
+					  <div class="card-body">
+					    <h5 class="card-title">Card title</h5>
+					    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					    <a href="#" class="btn btn-primary">Go somewhere</a>
+				  	</div>
+				</div>
+				</div>
+				<div class="col-md-6 col-lg-4  mb-4">
+					<div class="card">
+					  <img src="assets/img/imagen6.png" class="card-img-top" alt="...">
+					  <div class="card-body">
+					    <h5 class="card-title">Card title</h5>
+					    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					    <a href="#" class="btn btn-primary">Go somewhere</a>
+				  	</div>
+				</div>
+				</div>
+				<div class="col-md-6 col-lg-4  mb-4">
+					<div class="card">
+					  <img src="..." class="card-img-top" alt="...">
+					  <div class="card-body">
+					    <h5 class="card-title">Card title</h5>
+					    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					    <a href="#" class="btn btn-primary">Go somewhere</a>
+				  	</div>
+				</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- /Cards -->
 
 </main>
 <!-- /main -->
