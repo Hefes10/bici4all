a49f3ebe5f7c9301de1a20bce5285ebb0252bccc
hefes10 <martin.miret@gmail.com>
2019-04-03 15:51:41 -0300

    arreglados los enlaces

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'htpp://localhost/bici4all';
+$config['base_url'] = 'http://localhost/bici4all';
 
 /*
 |--------------------------------------------------------------------------
