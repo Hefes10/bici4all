922710a216999978db764e171e8c5b328adfe1c9
hefes10 <martin.miret@gmail.com>
2019-04-03 16:23:27 -0300

    enlaces

--- application/views/front/head_view.php
+++ application/views/front/head_view.php
@@ -6,11 +6,11 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<metahttp-equiv="X-UA-Compatible"content="IE=edge">
-		<link rel="stylesheet" type="text/css" href="assets/css/principal.css">
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/principal.css');?>">
 		<link href="https://fonts.googleapis.com/css?family=Fjalla+One|Source+Sans+Pro" rel="stylesheet">
 
 		<!-- Bootstrap CSS -->
-		<link href="assets/css/bootstrap.css" rel="stylesheet">
+		<link href="<?php echo base_url('assets/css/bootstrap.css');?>" rel="stylesheet">
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 		<!-- Bootstrap CDN -->
 		<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
