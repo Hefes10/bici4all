b3735407f0899c996b922144e2878e6fe9466292
hefes10 <martin.miret@gmail.com>
2019-04-03 18:42:56 -0300

    nada

--- application/views/contacto.php
+++ application/views/contacto.php
@@ -1,6 +1,6 @@
 <body>
   <main>
-    <div class="container">
+    <div id="contenedorBotella" class="container">
       <div class="row">
         <div class="col">
           <img src="assets/img/contacto.jpg" alt="botella">
