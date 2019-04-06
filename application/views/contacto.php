b5a44509e09e0bdbae41800dc9b98addaf99ffe0
hefes10 <martin.miret@gmail.com>
2019-04-06 19:01:44 -0300

    imagen de botella ordenado el css

--- application/views/contacto.php
+++ application/views/contacto.php
@@ -1,9 +1,7 @@
 <body>
   <main>
-    <div id="contenedorBotella" class="container">
-      <div class="row">
-        <div class="col">
-          <img src="assets/img/contacto.jpg" alt="botella">
+    <div id="contenedorBotella" class="container-fluid">
+          <img class="d-block w-100" src="assets/img/contacto.jpg" alt="botella">
         </div>
       </div>
     </div>
