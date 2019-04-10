608ef8e71f62b15129e239b0ff21bc6fd7c3b40e
hefes10 <martin.miret@gmail.com>
2019-04-09 21:40:56 -0300

    mejorado el contacto

--- application/views/contacto.php
+++ application/views/contacto.php
@@ -4,64 +4,91 @@
             <img class="d-block w-100" src="assets/img/contacto.jpg" alt="botella">
         </div>
 
-        <div id="formularioContacto" class="container">
+        <div class="container mt-2 mb-2">
             <div class="row">
-                <div class="col-md-8 ml-auto mr-auto">
-                    <div class="well well-sm">
-                        <form class="form-horizontal" method="post">
-                            <fieldset>
-                                <legend class="text-center header">Contáctanos</legend>
-                                <div class="form-group">
-                                    <span class="col-md-1 col-md-offset-2 text-center">
-                                        <i class="fa fa-user bigicon"></i>
-                                    </span>
-                                    <div class="col">
-                                        <input id="fname" name="name" type="text" placeholder="Nombre" class="form-control">
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <span class="col-md-1 col-md-offset-2 text-center">
-                                        <i class="fa fa-user bigicon"></i>
-                                    </span>
-                                    <div class="col">
-                                        <input id="lname" name="name" type="text" placeholder="Apellido" class="form-control">
-                                    </div>
-                                </div>
+                <div id="contacto" class="col">
+                    <h1>
+                        CONTACTO
+                    </h1>
+                    <br>
+                    <h4>
+                        SI TIENES ALGUNA INQUIETUD O COMENTARIO, NO DUDES EN CONTACTARNOS.
+                    </h4>
+                    <p>
+                        Puedes hacerlo completando el siguiente formulario, o si lo prefieres, puedes hacerlo escribiendo directamente a nuestra cuenta de mail: info@BC4ALL.com.ar
+                    </p>
+                    <br>
+                    <h4>
+                        BC4ALL - BICICLETAS ELECTRICAS
+                    </h4>
+                    <p>Showroom y Centro de Servicios</p>
+                    <p>Laprida 1171 Palermo, CP(1425)</p>
+                    <p>C.A.B.A.</p>
+                    <p>011-15-5800-8877</p>
+                    <p>011-4961-9561</p>
+                </div>
+                <div class="col">
+                    <div id="formularioContacto" class="container">
+                        <div class="row">
+                            <div class="col-md-8 ml-auto mr-auto">
+                                <div class="well well-sm">
+                                    <form class="form-horizontal" method="post">
+                                        <fieldset>
+                                            <legend class="text-center header">Contáctanos</legend>
+                                            <div class="form-group">
+                                                <span class="col-md-1 col-md-offset-2 text-center">
+                                                    <i class="fa fa-user bigicon"></i>
+                                                </span>
+                                                <div class="col">
+                                                    <input id="fname" name="name" type="text" placeholder="Nombre" class="form-control">
+                                                </div>
+                                            </div>
+                                            <div class="form-group">
+                                                <span class="col-md-1 col-md-offset-2 text-center">
+                                                    <i class="fa fa-user bigicon"></i>
+                                                </span>
+                                                <div class="col">
+                                                    <input id="lname" name="name" type="text" placeholder="Apellido" class="form-control">
+                                                </div>
+                                            </div>
 
-                                <div class="form-group">
-                                    <span class="col-md-1 col-md-offset-2 text-center">
-                                        <i class="fa fa-envelope-o bigicon"></i>
-                                    </span>
-                                    <div class="col">
-                                        <input id="email" name="email" type="text" placeholder="Email" class="form-control">
-                                    </div>
-                                </div>
+                                            <div class="form-group">
+                                                <span class="col-md-1 col-md-offset-2 text-center">
+                                                    <i class="fa fa-envelope-o bigicon"></i>
+                                                </span>
+                                                <div class="col">
+                                                    <input id="email" name="email" type="text" placeholder="Email" class="form-control">
+                                                </div>
+                                            </div>
 
-                                <div class="form-group">
-                                    <span class="col-md-1 col-md-offset-2 text-center">
-                                        <i class="fa fa-phone-square bigicon"></i>
-                                    </span>
-                                    <div class="col">
-                                        <input id="phone" name="phone" type="text" placeholder="Telefono" class="form-control">
-                                    </div>
-                                </div>
+                                            <div class="form-group">
+                                                <span class="col-md-1 col-md-offset-2 text-center">
+                                                    <i class="fa fa-phone-square bigicon"></i>
+                                                </span>
+                                                <div class="col">
+                                                    <input id="phone" name="phone" type="text" placeholder="Telefono" class="form-control">
+                                                </div>
+                                            </div>
 
-                                <div class="form-group ml-auto mr-auto">
-                                    <span class="col-md-1 col-md-offset-2 text-center">
-                                        <i class="fa fa-pencil-square-o bigicon"></i>
-                                    </span>
-                                    <div class="col">
-                                        <textarea class="form-control" id="message" name="message" placeholder="Ingrese su mensaje aquí. Nos pondremos en contacto con usted dentro de 2 días hábiles." rows="7"></textarea>
-                                    </div>
-                                </div>
+                                            <div class="form-group ml-auto mr-auto">
+                                                <span class="col-md-1 col-md-offset-2 text-center">
+                                                    <i class="fa fa-pencil-square-o bigicon"></i>
+                                                </span>
+                                                <div class="col">
+                                                    <textarea class="form-control" id="message" name="message" placeholder="Ingrese su mensaje aquí. Nos pondremos en contacto con usted dentro de 2 días hábiles." rows="7"></textarea>
+                                                </div>
+                                            </div>
 
-                                <div class="form-group">
-                                    <div class="col-md-12 text-center">
-                                        <button type="submit" class="btn btn-primary btn-lg">Enviar</button>
-                                    </div>
+                                            <div class="form-group">
+                                                <div class="col-md-12 text-center">
+                                                    <button type="submit" class="btn btn-primary btn-lg">Enviar</button>
+                                                </div>
+                                            </div>
+                                        </fieldset>
+                                    </form>
                                 </div>
-                            </fieldset>
-                        </form>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
