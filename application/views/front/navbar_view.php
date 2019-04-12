404e5da6979c348d0c70ec0c119fae922b2d6e16
hefes10 <martin.miret@gmail.com>
2019-04-11 22:30:23 -0300

    cambio en dropdown-menu

--- application/views/front/navbar_view.php
+++ application/views/front/navbar_view.php
@@ -16,9 +16,20 @@
 
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav ml-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">Productos<span class="sr-only">(current)</span></a>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Productos
+          </a>
+          <div id="dropdown-menu" class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">Bicicletas</a>
+            <a class="dropdown-item" href="#">Scooter</a>
+            <!-- <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </div> -->
           </li>
+          <!-- <li class="nav-item active">
+            <a class="nav-link" href="#">Productos<span class="sr-only">(current)</span></a>
+          </li> -->
           <li class="nav-item">
             <a class="nav-link" href="#">
               <i class="fa fa-user"></i> Login
@@ -29,17 +40,7 @@
               <i class="fa fa-shopping-cart"></i> Mi Carrito
             </a>
           </li>
-          <!-- <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Dropdown
-          </a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li> -->
+          
         </ul>
         <form class="form-inline my-2 my-lg-0">
           <input class="form-control mr-2" type="search" placeholder="¿Qué estás buscando?" aria-label="Search">
