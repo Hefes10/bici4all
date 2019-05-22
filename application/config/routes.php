5421269eba643fc3886096d79a77c87d6fea760b
hefes <martin.miret@gmail.com>
2019-05-22 19:57:20 -0300

    activa producto listo

--- application/config/routes.php
+++ application/config/routes.php
@@ -72,6 +72,7 @@ $route['productos_modifica'] = 'producto_controller/mustra_modificar';
 $route['productos_elimina'] = 'producto_controller/muestra_eliminar';
 $route['muestra_eliminados'] = 'producto_controller/muestra_eliminados';
 $route['productos_elimina/(:num)'] = 'producto_controller/eliminar_producto/$1';
+$route['productos_activa/(:num)'] = 'producto_controller/activar_producto/$1';
 
 //productos
 
