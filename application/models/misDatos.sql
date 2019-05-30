acf5094b705d89c37ea6d9f9d98abfb10c3d81c1
hefes <martin.miret@gmail.com>
2019-05-30 19:34:07 -0300

    datos corregidos

--- application/models/misDatos.sql
+++ application/models/misDatos.sql
@@ -11,12 +11,12 @@ INSERT INTO categorias VALUES
 (2, 'scooter');
 
 INSERT INTO productos VALUES
-(1, 'bicicleta1', 1, 1000.00, 1500.00, 10, 1, '', 'NO'),
-(2, 'bicicleta2', 1, 1000.00, 1500.00, 10, 1, '', 'NO'),
-(3, 'bicicleta3', 1, 1000.00, 1500.00, 10, 1, '', 'NO'),
-(4, 'bicicleta4', 1, 1000.00, 1500.00, 10, 1, '', 'NO'),
-(5, 'scooter1', 2, 1000.00, 1500.00, 10, 1, '', 'NO'),
-(6, 'scooter2', 2, 1000.00, 1500.00, 10, 1, '', 'NO'),
-(7, 'scooter3', 2, 1000.00, 1500.00, 10, 1, '', 'NO'),
-(8, 'scooter4', 2, 1000.00, 1500.00, 10, 1, '', 'NO'),
-(9, 'scooter5', 2, 1000.00, 1500.00, 10, 1, '', 'NO');
+(1, 'bicicleta1', 1, 1000.00, 1500.00, 10, 1, 'assets/img/productos/imagen1.jpg', 'NO'),
+(2, 'bicicleta2', 1, 1000.00, 1500.00, 10, 1, 'assets/img/productos/imagen2.jpg', 'NO'),
+(3, 'scooter1', 2, 1000.00, 1500.00, 10, 1, 'assets/img/productos/imagen3.png', 'NO'),
+(4, 'bicicleta4', 1, 1000.00, 1500.00, 10, 1, 'assets/img/productos/imagen4.png', 'NO'),
+(5, 'bicicleta5', 1, 1000.00, 1500.00, 10, 1, 'assets/img/productos/imagen5.jpg', 'NO'),
+(6, 'scooter2', 2, 1000.00, 1500.00, 10, 1, 'assets/img/productos/imagen6.png', 'NO'),
+(7, 'bicicleta6', 1, 1000.00, 1500.00, 10, 1, 'assets/img/productos/imagen7.jpg', 'NO'),
+(8, 'scooter4', 2, 1000.00, 1500.00, 10, 1, 'assets/img/productos/imagen8.png', 'NO'),
+(9, 'bicicleta7', 1, 1000.00, 1500.00, 10, 1, 'assets/img/productos/imagen21.jpg', 'NO');
